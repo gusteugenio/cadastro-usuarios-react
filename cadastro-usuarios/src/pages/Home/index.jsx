1ffddf73a36b2f51802e3dd868db087c8d3d659c
gusteugenio <gustavotertuliano@chuva-inc.com>
2024-07-29 01:16:25 -0300

    feat: Cria cadastro de usuários

---
+++ cadastro-usuarios/src/pages/Home/index.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState, useRef } from "react";
+import "./style.css";
+import Trash from "../../assets/trash.svg";
+import api from '../../services/api'
+
+function Home() {
+  const [users, setUsers] = useState([])
+
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+  
+  async function getUsers(){
+    const usersFromApi = await api.get('/usuarios')
+
+    setUsers(usersFromApi.data)
+  }
+
+  async function createUsers(){
+
+    const userData = {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value
+    };
+    
+    try {
+      await api.post('/usuarios', userData);
+      getUsers();
+    } catch (error) {
+      console.error("Erro ao criar usuário:", error);
+    }
+  }
+
+  async function deleteUsers(id){    
+    await api.delete(`/usuarios/${id}`)
+    getUsers()
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+
+  return (
+    <div className="container">
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input placeholder="Nome" name="nome" type="text" ref={inputName}/>
+        <input placeholder="Idade" name="idade" type="number" ref={inputAge}/>
+        <input placeholder="Email" name="email" type="email" ref={inputEmail}/>
+        <button type="button" onClick={createUsers}>Cadastrar</button>
+      </form>
+
+      {users.map((user) => (
+        <div key={user.id} className="card">
+          <div>
+            <p>Nome: <span>{user.name}</span></p>
+            <p>Idade: <span>{user.age}</span></p>
+            <p>Email: <span>{user.email}</span></p>
+          </div>
+            <button onClick={() => deleteUsers(user.id)}>
+              <img src={Trash}></img>
+            </button>
+        </div>
+      ))}
+
+    </div>
+  );
+}
+
+export default Home;
